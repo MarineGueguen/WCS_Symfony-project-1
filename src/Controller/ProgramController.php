6cf1be0b2c51470aff5e532a0289f548174f66e9
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-05-26 16:34:46 +0200

    quest 11 - show episodes by season

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EpisodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,10 +25,27 @@ class ProgramController extends AbstractController
     public function show(int $id, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
     {
         $program = $programRepository->findOneBy(['id' => $id]);
+        if (!$program) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
         $seasons = $seasonRepository->findBy(['program' => $program->getId()]);
         return $this->render('program/show.html.twig', [
             'program' => $program,
             'seasons' => $seasons,
+        ]);
+    }
+
+    #[Route('/program/{programId}/season/{seasonId}', requirements: ['programId'=>'\d+', 'seasonId'=>'\d+'], methods: ['GET'], name: 'program_season_show')]
+    public function showSeason(int $programId, int $seasonId, SeasonRepository $seasonRepository, EpisodeRepository $episodeRepository): Response
+    {
+        $season = $seasonRepository->findOneBy(['program' => $programId, 'id' => $seasonId]);
+        if (!$season) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+        $episodes = $episodeRepository->findBy(['season' => $season->getId()]);
+        return $this->render('program/season_show.html.twig', [
+            'season' => $season,
+            'episodes' => $episodes,
         ]);
     }
 }
