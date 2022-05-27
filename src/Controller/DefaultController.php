161f1371499d1f7c2044129b98aac42bf6621d0f
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-05-27 16:59:34 +0200

    supression of additional </h1> in index and removed an empty line in default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-
     public function index(): Response
     {
         return $this->render('index.html.twig', [
