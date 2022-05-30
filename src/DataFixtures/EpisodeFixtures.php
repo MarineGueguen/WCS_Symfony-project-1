cea49a0ea253bd5c0358bfbd50a35f267d7e3347
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-05-30 10:35:02 +0200

    modification of fixtures so that episodes number be coherent

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -56,283 +56,284 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
-        for($i = 0; $i < 10; $i++) {
+        
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_The Uncanny Counter_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_The Uncanny Counter_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_The Uncanny Counter_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_The Uncanny Counter_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_The Uncanny Counter_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Criminal Minds_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Criminal Minds_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Criminal Minds_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Criminal Minds_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Criminal Minds_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_LOVE DEATH + ROBOTS_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_LOVE DEATH + ROBOTS_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_LOVE DEATH + ROBOTS_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_LOVE DEATH + ROBOTS_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_LOVE DEATH + ROBOTS_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         } 
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Strong Woman Bong Soon_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Strong Woman Bong Soon_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Strong Woman Bong Soon_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Strong Woman Bong Soon_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Strong Woman Bong Soon_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Doom at your service_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Doom at your service_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Doom at your service_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Doom at your service_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Doom at your service_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Kingdom_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Kingdom_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Kingdom_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Kingdom_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Kingdom_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Descendants of the Sun_season_1'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Descendants of the Sun_season_2'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Descendants of the Sun_season_3'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Descendants of the Sun_season_4'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
-        for($i = 0; $i < 10; $i++) {
+        for($i = 1; $i < 11; $i++) {
             $episode = new Episode();
             $episode->setSeason($this->getReference('program_Descendants of the Sun_season_5'));
             $episode->setTitle($faker->sentences(1, true));
-            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setNumber($i);
             $episode->setSynopsis($faker->paragraphs(1, true));
             $manager->persist($episode);
         }
