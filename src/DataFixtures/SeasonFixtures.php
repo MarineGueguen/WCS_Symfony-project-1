080694b3dc81aa33d90c63a8a603f7f042216955
MARINE GUEGUEN <gueguenmarine.p7@gmail.com>
2022-05-30 10:45:55 +0200

    modification of fixtures so that seasons number be coherent

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -53,70 +53,63 @@ class SeasonFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);
+            $season->setNumber($i);
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_The Uncanny Counter'));
             $manager->persist($season);
             $this->addReference('program_The Uncanny Counter_season_' . $season->getNumber(), $season);
         }
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);
+            $season->setNumber($i);
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_Criminal Minds'));
             $manager->persist($season);
             $this->addReference('program_Criminal Minds_season_' . $season->getNumber(), $season);
         }
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);            
+            $season->setNumber($i);            
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_LOVE DEATH + ROBOTS'));
             $manager->persist($season);
             $this->addReference('program_LOVE DEATH + ROBOTS_season_' . $season->getNumber(), $season);
         }
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);            
+            $season->setNumber($i);            
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_Strong Woman Bong Soon'));
             $manager->persist($season);
             $this->addReference('program_Strong Woman Bong Soon_season_' . $season->getNumber(), $season);
         }
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);            
+            $season->setNumber($i);            
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_Doom at your service'));
             $manager->persist($season);
             $this->addReference('program_Doom at your service_season_' . $season->getNumber(), $season);
         }
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);            
+            $season->setNumber($i);            
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_Kingdom'));
             $manager->persist($season);
             $this->addReference('program_Kingdom_season_' . $season->getNumber(), $season);
         }
-        $number = 1;
-        for($i = 0; $i < 5; $i++) {
+        for($i = 1; $i < 6; $i++) {
             $season = new Season();
-            $season->setNumber($number++);
+            $season->setNumber($i);
             $season->setYear($faker->year());
             $season->setDescription($faker->paragraphs(3, true));
             $season->setProgram($this->getReference('program_Descendants of the Sun'));
